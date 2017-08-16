2d8aba63dc78045c44119f1ebd66ac4efc7b692e
rubyist <scott.barron@github.com>
2017-08-15 21:01:13 -0400

    add a quick cache to the proxy

--- proxy/main.go
+++ proxy/main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+// This crap is rate limited to somehing weak like 9 reqs per minute.
+const perMinute = 9
+
+var (
+	cnt       = int64(0)
+	caches    = make(map[string]bytes.Buffer)
+	cacheLock sync.Mutex
 )
 
 type h struct{}
 
 func (h) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	cacheKey := fmt.Sprintf("%s:%s", r.URL.Path, r.URL.Query().Get("guid"))
+
 	w.Header().Add("Access-Control-Allow-Origin", "*")
+
+	if atomic.LoadInt64(&cnt) >= perMinute {
+		log.Printf("serving cached response for %s", cacheKey)
+		cacheLock.Lock()
+		buf := caches[cacheKey]
+		io.Copy(w, &buf)
+		cacheLock.Unlock()
+		return
+	}
 
 	req, err := http.NewRequest("GET", fmt.Sprintf("https://realmofthemadgodhrd.appspot.com%s", r.URL.String()), nil)
 	if err != nil {
@@ -26,9 +50,25 @@ func (h) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer resp.Body.Close()
-	io.Copy(w, resp.Body)
+
+	cacheLock.Lock()
+	buf := caches[cacheKey]
+	buf.Reset()
+	tee := io.TeeReader(resp.Body, &buf)
+	io.Copy(w, tee)
+	caches[cacheKey] = buf
+	cacheLock.Unlock()
+	atomic.AddInt64(&cnt, 1)
+	log.Printf("freshly served %s", cacheKey)
 }
 
 func main() {
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+			atomic.StoreInt64(&cnt, 0)
+		}
+	}()
+
 	log.Fatal(http.ListenAndServe(":5353", h{}))
 }
